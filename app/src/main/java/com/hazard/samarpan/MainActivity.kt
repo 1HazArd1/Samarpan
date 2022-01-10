1df36b157cc700cec045f2c99e037c6ab1511aa3
HazArd <aakashgoingto99@gmail.com>
2022-01-10 21:40:50 +0530

    Splash Screen added

--- app/src/main/java/com/hazard/samarpan/MainActivity.kt
+++ app/src/main/java/com/hazard/samarpan/MainActivity.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        /*setTheme(R.style.Theme_Samarpan)
+           This part of the code is used to set the default theme as the theme was set to the splash screen before setting the main activity layout
+           remove the comment tag for adding the logic*/
         setContentView(R.layout.activity_main)
     }
 }
