e81388288520f33b3ca8ef67fd891ab11cb1e0c5
HazArd <aakashgoingto99@gmail.com>
2022-01-21 02:11:40 +0530

    Did few changes in user login screen

--- app/src/main/java/com/hazard/samarpan/MainActivity.kt
+++ app/src/main/java/com/hazard/samarpan/MainActivity.kt
@@ -1,14 +1,23 @@
 package com.hazard.samarpan
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*setTheme(R.style.Theme_Samarpan)
-           This part of the code is used to set the default theme as the theme was set to the splash screen before setting the main activity layout
+        if(Build.VERSION.SDK_INT < 16){
+            window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
+        window.decorView.systemUiVisibility= View.SYSTEM_UI_FLAG_FULLSCREEN
+        actionBar?.hide()
+        /*This part of the code is used to set the default theme as the theme was set to the splash screen before setting the main activity layout
            remove the comment tag for adding the logic*/
+        setTheme(R.style.Theme_Samarpan)
         setContentView(R.layout.activity_main)
     }
 }
