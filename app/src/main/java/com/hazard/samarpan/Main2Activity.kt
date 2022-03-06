14368ef47da16afa98621f17925ccf7e0d7b4194
Suswan114 <infinityplaycons04@gmail.com>
2022-03-06 21:37:49 +0530

    Dashboard Page UI

--- app/src/main/java/com/hazard/samarpan/Main2Activity.kt
+++ app/src/main/java/com/hazard/samarpan/Main2Activity.kt
@@ -8,21 +8,27 @@ import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class Main2Activity : AppCompatActivity() {
-    private var btnLogout: Button? =null
+//    private var btnLogout: Button? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Samarpan)
         setContentView(R.layout.activity_main2)
+        addFragment()
 
         //this is just for testing the logout functionality remove this while adding the dashboard fragment
-        btnLogout=findViewById(R.id.btnLogout)
+//        btnLogout=findViewById(R.id.btnLogout)
+//
+//        btnLogout?.setOnClickListener{
+//            Firebase.auth.signOut()
+//            val intent=Intent(this,MainActivity::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
 
-        btnLogout?.setOnClickListener{
-            Firebase.auth.signOut()
-            val intent=Intent(this,MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }
     }
-
+    private fun addFragment() {
+        val dashboard=DashboardFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container2,dashboard).commit()
+    }
 }
